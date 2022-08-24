f5e2b5c5e11d8d73e01d5aeaed1a616865d0bdef
ittuki <ittuki@kukkula.local>
2022-08-24 09:15:55 +0300

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", textAlign: "center"}}>
+      <h1>Ruokahintalaskuri</h1>
+      <ul>
+      <div style={{backgroundColor: "black", color:"lightgray", width: 350, marginLeft:"37%"}}>
+        <h2> X maksaa tänään x.xx X</h2>
+      </div>
+      </ul>
+    </div>
+  );
+}
